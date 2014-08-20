1dff21d24f962645e1e6fa7ca84ddec0e9e18ef0
Luke Rebarchik <myfavoriteluke@gmail.com>
2014-08-20 09:32:43 -0600

    moved some scss and dealt with click-spam bug
    When triggering animation while animation is in process caused the id to
    be deleted and broke the code.

--- js/animateReplace.jquery.js
+++ js/animateReplace.jquery.js
@@ -41,7 +41,7 @@ Animate and Replace (animateReplace for short)
 						new Plugin( _this, $.extend(true, defaults, options) )
 					);
 					//console.log($(_this).attr("id"));
-					if($(_this).attr("id") == "") {
+					if($(_this).attr("id") == "" || $(_this).attr("id") == "undefined") {
 						//console.log("adding id");
 						data['id-added'] = true;
 						$(_this).attr("id", "ar-id"+parseInt($(_this).text(), 36).toString(16));
@@ -57,10 +57,12 @@ Animate and Replace (animateReplace for short)
 			'put-data': function ( _this, data ) {
 				methods['debug']( _this, "put-data:", "put-data" );
 				methods['debug']( _this, data, "put-data" );
-				return $.data( _this, 'plugin_' + pluginName, data );
+				return $.data( _this, 'plugin_' + pluginName, $.extend(true, methods['get-data']( _this ), data ));
 			},
 			'get-data': function ( _this ) {
 				var data = $.data( _this, 'plugin_' + pluginName);
+				methods['debug']( _this, "put-data:", "get-data" );
+				methods['debug']( _this, data, "get-data" );
 				return data || methods['init-data']( _this, arguments[1] );
 			},
 			'move-data': function ( _this, to_this ) {
@@ -102,7 +104,7 @@ Animate and Replace (animateReplace for short)
 			},
 			
 			/****
-			 * Begin main [non-generic] type methods
+			 * Begin main [non-generic] methods
 			 */
 			
 			flip: function( _this, args ) {
@@ -112,75 +114,89 @@ Animate and Replace (animateReplace for short)
 					flipTo = args[0] || data.flips[0],
 					id = $this.attr("id")
 				;
-				//	In order for the CSS transitions to work the parent of the element needs some specific classes.
-				//	There are 2 basic options here;
-				//		1) "flip" from the parent.
-				//		2) transparently add a parent to the DOM, inheriting the dimensions of the element
-				//	Once that is known, then we will add the new element and apply the CSS transition.
-				//	Based on the CSS transition timing, the now hidden content will be removed and stored in the $.data of the parent.
-				//	
-				//	If no element is provided to "flip" to, then there must be an element stored.
-				//	The first (index:0) element stored will be used as the new "flip" content.
 				
-				methods['debug']( _this, "Flipping element!", "flip");
-				
-				//	This part allows for either option listed above (1 or 2)
-				if( !data.options['anchor'] || data.options['anchor'].toLowerCase() !== "parent") {
-					//	1
-					var anchor = $('<div style="display:inline-block;margin:0;padding:0;border:0"></div>').attr("id", "arparent-id"+$this.attr("id"));
-					anchor.css("height",$this.height());
-					anchor.css("width",$this.width());
-					anchor.insertAfter($this);
-					anchor.data($this.data());
-					anchor.append($this);
-					console.log("anchor:");
-					console.log($this);
-					console.log(anchor);
-					//$this = anchor;
-					methods['debug']( _this, "option 1", "flip");
-				} else {
-					//	2
-					var anchor = $this.parent();
-					//$this = anchor;
-					methods['debug']( _this, "option 2", "flip");
-				}
-				//	Now verify that flipTo is valid
-				if( flipTo instanceof HTMLElement )	flipTo = $(flipTo);
-				if( !(flipTo instanceof $) ) {
+				if((data['flip-to'] == undefined && !$this.hasClass("animated")) || data === null ) {
+					methods['debug']( _this, "Not flipping already...", "flip");
+					
+					//	In order for the CSS transitions to work the parent of the element needs some specific classes.
+					//	There are 2 basic options here;
+					//		1) "flip" from the parent.
+					//		2) transparently add a parent to the DOM, inheriting the dimensions of the element
+					//	Once that is known, then we will add the new element and apply the CSS transition.
+					//	Based on the CSS transition timing, the now hidden content will be removed and stored in the $.data of the parent.
+					//	
+					//	If no element is provided to "flip" to, then there must be an element stored.
+					//	The first (index:0) element stored will be used as the new "flip" content.
+					
+					methods['debug']( _this, "Flipping element!", "flip");
+					data = methods['put-data']( _this, {flipping: true} );
+					methods['debug']( _this, data, "flip");
+					//	This part allows for either option listed above (1 or 2)
+					if( !data.options['anchor'] || data.options['anchor'].toLowerCase() !== "parent") {
+						//	1
+						var anchor = $('<div style="display:inline-block;margin:0;padding:0;border:0"></div>').attr("id", "arparent-id"+$this.attr("id"));
+						anchor.css("height",$this.height());
+						anchor.css("width",$this.width());
+						anchor.insertAfter($this);
+						anchor.data($this.data());
+						anchor.append($this);
+						methods['debug']( _this, "anchor:", "flip");
+						methods['debug']( _this, $this, "flip");
+						methods['debug']( _this, anchor, "flip");
+						//$this = anchor;
+						methods['debug']( _this, "option 1", "flip");
+					} else {
+						//	2
+						var anchor = $this.parent();
+						//$this = anchor;
+						methods['debug']( _this, "option 2", "flip");
+					}
+					//	Now verify that flipTo is valid
+					if( flipTo instanceof HTMLElement )	flipTo = $(flipTo);
+					if( !(flipTo instanceof $) ) {
+						methods['debug']( _this, flipTo, "flip");
+						$.error("No valid element provided to flip to!");
+					}
+					
+					//console.log($this.children().first());
 					methods['debug']( _this, flipTo, "flip");
-					$.error("No valid element provided to flip to!");
+					data['flip-to'] = flipTo;
+					
+					anchor.addClass("appear-animation");
+					//$this.addClass("animated");
+					anchor.children().first().removeClass("coming-in");
+					anchor.children().first().removeClass("going-out");
+					flipTo.removeClass("coming-in");
+					flipTo.removeClass("going-out");
+					
+					//$this.children().first().addClass("going-out");
+					//flipTo.addClass("coming-in");
+					
+					anchor.children().first().addClass("coming-in");
+					flipTo.addClass("going-out");
+					
+					flipTo.insertAfter(anchor.children().first());
+					
+					if(!data['flipped-out']) {
+						data['flipped-out'] = $("<span></span>");
+					}
+					var toEval_var = 'var d=$("#'+anchor.attr("id")+'").data("plugin_'+pluginName+'"),e=d["flip-to"],p=e.parent();',
+						toEval1 = /*toEval_var+*/'$("#'+anchor.attr("id")+'").addClass("animated");';
+						toEval2 = toEval_var+'e.removeClass("going-out");e.removeClass("coming-in");e.insertAfter(p);d["flipped-out"].append(p);e.data("plugin_'+pluginName+'", null);';	//console.log("end");console.log(e.data("plugin_'+pluginName+'"));
+						//	/*d["replaced"]=;e.data("plugin_'+pluginName+'", d);*/
+					methods['debug']( _this, toEval1, "flip");
+					methods['debug']( _this, toEval2, "flip");
+					window.setTimeout(toEval1, 0);
+					window.setTimeout(toEval2, data.options['timing-duration']*1000+250);
+				} else {
+					methods['debug']( _this, "Mid-flip, problems?...", "flip");
+					methods['debug']( _this, data['flip-to'] == undefined, "flip");
+					methods['debug']( _this, !$this.hasClass("animated"), "flip");
+					methods['debug']( _this, data === undefined, "flip");
+					methods['debug']( _this,  data , "flip");
+					methods['debug']( _this,  _this , "flip");
+					methods['debug']( _this,  $this.data("plugn_"+pluginName) , "flip");
 				}
-				
-				//console.log($this.children().first());
-				console.log(flipTo);
-				data['flip-to'] = flipTo;
-				
-				anchor.addClass("appear-animation");
-				//$this.addClass("animated");
-				anchor.children().first().removeClass("coming-in");
-				anchor.children().first().removeClass("going-out");
-				flipTo.removeClass("coming-in");
-				flipTo.removeClass("going-out");
-				
-				//$this.children().first().addClass("going-out");
-				//flipTo.addClass("coming-in");
-				
-				anchor.children().first().addClass("coming-in");
-				flipTo.addClass("going-out");
-				
-				flipTo.insertAfter(anchor.children().first());
-				
-				if(!data['flipped-out']) {
-					data['flipped-out'] = $("<span></span>");
-				}
-				var toEval_var = 'var d=$("#'+anchor.attr("id")+'").data("plugin_'+pluginName+'"),e=d["flip-to"],p=e.parent();',
-					toEval1 = /*toEval_var+*/'$("#'+anchor.attr("id")+'").addClass("animated");';
-					toEval2 = toEval_var+'e.removeClass("going-out");e.removeClass("coming-in");e.insertAfter(p);d["flipped-out"].append(p);';
-					//	/*d["replaced"]=;e.data("plugin_'+pluginName+'", d);*/
-				methods['debug']( _this, toEval1, "flip");
-				methods['debug']( _this, toEval2, "flip");
-				window.setTimeout(toEval1, 0);
-				window.setTimeout(toEval2, data.options['timing-duration']*1000+250);
 			}
 			
 			/****
